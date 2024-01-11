fecbaa23c42598f5f0f7b667d5cab48018f05765
Omo <omoikheloa56@gmail.com>
2024-01-10 18:59:08 -0800

    v2

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,27 +1,30 @@
 #!/usr/bin/python3
-def roman_value(character):
-    roman_dict = {'I': 1, 'V': 5, 'X': 10,
-            'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    return roman_dict.get(character, 0)
+def roman_value(prmCharacter):
+    roman_list = {'I': 1, 'V': 5, 'X': 10,
+                  'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    return roman_dict.get(prmCharacter, None)
 
-def next_value(roman_string, index):
-    if index + 1 < len(roman_string):
-        return roman_value(roman_string[index + 1])
+
+def next_value(prmString, prmIndex):
+    if prmIndex + 1 < len(prmString):
+        return roman_value(prmString[prmIndex + 1])
     else:
-        return 0
+        return None
+
 
 def roman_to_int(roman_string):
-    if not isinstance(roman_string, str) or not all(char.upper() in 'IVXLCDM' for char in roman_string):
-        return 0
-
     result = 0
-    for index, character in enumerate(roman_string):
-        current_value = roman_value(character)
-        next_value_ = next_value(roman_string, index)
 
-        if current_value >= next_value_:
-            result += current_value
+    if (roman_string is None or isinstance(roman_string, str) is False):
+        return result
+
+    enum = enumerate(roman_string)
+    for index, character in enum:
+        currentValue = roman_value(character)
+        nextValue = next_value(roman_string, index)
+        if nextValue is None or currentValue >= nextValue:
+            result += currentValue
         else:
-            result += (next_value_ - current_value)
-
+            result += (nextValue - currentValue)
+            next(enum)
     return result
