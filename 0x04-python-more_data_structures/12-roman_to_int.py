d16cc2cec37de4a18891f190406cae167353a498
Omo <omoikheloa56@gmail.com>
2024-01-10 19:01:28 -0800

    v3

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
 def roman_value(prmCharacter):
-    roman_list = {'I': 1, 'V': 5, 'X': 10,
-                  'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    return roman_dict.get(prmCharacter, None)
+    roman_list = [('I', 1), ('V', 5), ('X', 10),
+                  ('L', 50), ('C', 100), ('D', 500), ('M', 1000)]
+    for item in roman_list:
+        character, value = item
+        if (prmCharacter is character):
+            return value
+    return None
 
 
 def next_value(prmString, prmIndex):
