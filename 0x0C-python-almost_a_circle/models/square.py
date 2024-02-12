bfb598b999a97fe4605fec33dd2cfcd178a35a6a
Omo <omoikheloa56@gmail.com>
2024-02-12 13:47:56 -0800

    pycodestyle square

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -6,6 +6,7 @@ from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
+    """square implements rectangle"""
     def __init__(self, size, x=0, y=0, id=None):
         """Initialize a Square object."""
         super().__init__(size, size, x, y, id)
@@ -18,12 +19,19 @@ class Square(Rectangle):
     @size.setter
     def size(self, value):
         """Setter for the size attribute."""
+        if type(value) != int:
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width ,ust be > 0")
+
         self.width = value
         self.height = value
 
     def __str__(self):
         """Return a string representation of the square."""
-        return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y, self.width)
+        return "[{}] ({}) {}/{} - {}".format(type(self).__name__,
+                                             self.id, self.x, self.y,
+                                             self.width)
 
     def update(self, *args, **kwargs):
         """Assigns attributes using *args and **kwargs."""
@@ -40,9 +48,7 @@ class Square(Rectangle):
 
     def to_dictionary(self):
         """Return the dictionary representation of a Square."""
-        return {
-            'id': self.id,
-            'size': self.size,
-            'x': self.x,
-            'y': self.y
-        }
+        return {'id': getattr(self, "id"),
+                'size': getattr(self, "width"),
+                'x': getattr(self, "x"),
+                'y': getattr(self, "y")}
