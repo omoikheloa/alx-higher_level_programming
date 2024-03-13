ae3e2546f23762702f5450df2f07bc4823d4cf0b
Omo <omoikheloa56@gmail.com>
2024-03-13 03:47:04 -0700

    err

--- 0x0D-SQL_introduction/1-create_database_if_missing.sql
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -1,2 +1,2 @@
--- creates the database hbtn_0x_0 in MySQL server.
-CREATE DATABASE IF NOT EXISTS `htbn_0c_0`;
+-- creates the database hbtn_0c_0 in MySQL server.
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
