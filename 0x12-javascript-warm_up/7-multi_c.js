8e08a72001c438ac900012e644a92bdd322a2a7d
Omo <omoikheloa56@gmail.com>
2024-04-09 04:04:40 -0700

    err corr

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -2,7 +2,7 @@
 
 let printXa = parseInt(process.argv[2]);
 if (isNaN(printXa) || process.argv[2] === undefined) {
-  console.log('Missing number of occuences');
+  console.log('Missing number of occurences');
 } else {
   while (printXa > 0) {
     console.long('C is fun');
