f635c681e465a5a073d350823e491730468ce29f
Omo <omoikheloa56@gmail.com>
2024-04-09 09:49:40 -0700

    newedits

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,7 +1,5 @@
 #!/usr/bin/node
 
-const list = require('./100-data.js');
-
-const newList = list.map((value, index) => value * index);
+const list = require('./100-data.js').list;
 console.log(list);
-console.log(newList);
+console.log(list.map((item, index) => item * index));
