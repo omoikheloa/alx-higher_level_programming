d87762ad87b27803cad077df2ff8fb6d87032707
Omo <omoikheloa56@gmail.com>
2024-04-09 04:15:35 -0700

    more changes

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 
-let printXa = parseInt(process.argv[2]);
-if (isNaN(printXa) || process.argv[2] === undefined) {
+let printXt = parseInt(process.argv[2]);
+if (isNaN(printXt) || process.argv[2] === undefined) {
   console.log('Missing number of occurences');
 } else {
-  while (printXa > 0) {
+  while (printXt > 0) {
     console.long('C is fun');
-    printXa--;
+    printXt--;
   }
 }
